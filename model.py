e01ed8050b122d954914725175f254bc9842a6d6
Bless <tetaangelbless014@gmail.com>
2025-10-23 22:05:07 +0200

    Initial commit

--- model.py
+++ model.py
@@ -17,23 +17,25 @@ context = " ".join([item["answer"] for item in qa_data])
 qa_pipeline = pipeline("question-answering", model=model, tokenizer=tokenizer)
 
 # Run a loop to ask questions
-
 while True:
-    choice=input("1.Gukomeza kubaza: 00.Gusubira inyuma: 3.Gusohokamo")
-if choice == "1":
-    while True:
-    question = input("Shyiramo ikibazo: ") 
-    if question == "0":
-        break
-    result = qa_pipeline(question=question, context=context)
-    print("Igisubizo:", result['answer'],"\n")
-     elif choice == "2":
+    choice = input("1. Gukomeza kubaza | 2. Gusubira inyuma | 3. Gusohokamo: ")
+
+    if choice == "1":
+        while True:
+            question = input("Shyiramo ikibazo (andika '0' kugira usubire inyuma): ")
+            if question == "0":
+                break
+            result = qa_pipeline(question=question, context=context)
+            print("Igisubizo:", result['answer'], "\n")
+
+    elif choice == "2":
         print("Wahisemo gusubira inyuma...")
-        # Aha ushobora gushyiraho indi menu cyangwa ibikorwa byasubirwamo.
+        # Aha ushobora gushyiraho indi menu cyangwa ibikorwa byasubirwamo
         continue
-     elif choice == "3":
+
+    elif choice == "3":
         print("Murakoze! Muri gusohoka...")
         break
-     else:
-        print("Amahitamo ntakwiye, ongera ugerageze.\n")
 
+    else:
+        print("Amahitamo ntakwiye, ongera ugerageze.\n")
